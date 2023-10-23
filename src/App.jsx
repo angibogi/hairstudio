be51c12499aa5f10bb4d7a021e497f47525a786d
Angelo <a.pisano13@campus.unimib.it>
2023-10-24 01:49:03 +0200

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-import { useState } from 'react'
+import React, { useRef,useState} from 'react'
 import logo from './assets/logo.png'
 import './App.css'
+import emailjs from '@emailjs/browser';
+import {Helmet} from "react-helmet";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const form = useRef();
+    const [done, setDone] = useState(false);
+    const [label, setLabel] = useState(true);
+
+    
+    
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_2kl80zv', 'template_iiadgy2', form.current, 'd5XJP6UfiJqr7E5vQ')
+        .then((result) => {
+          console.log(result.text);
+          setDone(true);
+          setLabel(false);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+        
+        );
+        e.target.reset();
+    };
 
   return (
     <>
+        <Helmet>
+                <meta charSet="utf-8" />
+                <title>Hair Studio - Newsletter</title>
+                <link rel="canonical" href="http://mysite.com/example" />
+          </Helmet>
     <div class="contact_image">
     <img src={logo} alt="logo"/>
     </div>
@@ -14,12 +42,13 @@ function App() {
     <h1>Iscriviti per ricevere sconti personalizzati!</h1>
     </div>
 
-    <form>
+    <form ref={form} onSubmit={sendEmail} >
+    {label ?
         <div class="contact_box">
         <label for="name">Nome:</label>
         <input type="text" id="name" name="name" required/>
 
-        <label for="cognome">Cognome:</label>
+        <label for="surname">Cognome:</label>
         <input type="text" id="cognome" name="cognome" required/>
 
         <label for="birthdate">Data di nascita:</label>
@@ -31,14 +60,13 @@ function App() {
         <label for="phone">Numero di telefono:</label>
         <input type="tel" id="phone" name="phone" required/>
         </div>
+                : null}
+                 {done ? <div><p>La ringraziamo per averci contattato, stiamo elaborando la sua richiesta </p> </div>  : null}
     </form>
 
     <div class="contact_box">
     <button type="submit">Invia</button>
     </div>
-
-
-
     </>
   )
 }
